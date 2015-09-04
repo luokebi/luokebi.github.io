e0ebb4e1e51f6f2f6d3c0a5862dfca10ee76323d
LUO <luo@LUOdeMacBook-Pro.local>
2015-09-04 21:24:02 +0800

    some updates

--- exam/js/input.js
+++ exam/js/input.js
@@ -7,22 +7,32 @@ function generateJson() {
 	var _content = $('#test-input').val().replace(/\n+|\r+|\s+/g,'');
 
 	var content = _content.split(/A\./)[0];
-	var A = _content.match(/A\.(.*)B\./)[1];
-	var B = _content.match(/B\.(.*)C\./)[1];
-	var C = _content.match(/C\.(.*)D\./)[1];
-	var D = _content.match(/D\.(.*)$/)[1];
+	var anwser = _content.match(/[（|()]([A-D]+)[）|\)]/)[1];
+	var type = $('#type-select').val();
+	if (type == 'single2') {
+		var obj = {
+			type: 'single2',
+			content: content,
+			answer: anwser
+		};
+	} else {
+		var A = _content.match(/A\.(.*)B\./)[1];
+		var B = _content.match(/B\.(.*)C\./)[1];
+		var C = _content.match(/C\.(.*)D\./)[1];
+		var D = _content.match(/D\.(.*)$/)[1];
 
-	var answer = _content.match(/[（|()]([A-D]+)[）|\)]/)[1];
+		var obj = {
+		    type: type,
+		    content: content,
+		    A: A,
+		    B: B,
+		    C: C,
+		    D: D,
+		    answer: anwser
+		};
 
-	var obj = {
-		type: $('#type-select').val(),
-		content: content,
-		A: A,
-		B: B,
-		C: C,
-		D: D,
-		answer: answer
 	}
+	
 
 	$('#output').text(JSON.stringify(obj));
 
