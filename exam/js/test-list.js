844ed9701d6f4bf72c13286c5bac5b62f1fbdbde
LUO <luo@LUOdeMacBook-Pro.local>
2015-09-05 00:50:56 +0800

    some updates

--- exam/js/test-list.js
+++ exam/js/test-list.js
@@ -1,12 +1,27 @@
+if (window.location.search && window.location.search.match(/is_back=(.*)/)) {
+    var isBack = window.location.search.match(/is_back=(.*)/)[1];
+    if (isBack == 'true') {
+        $('.loading-area').hide();
+    }
+}
+
+
+
+
+$('#enter-btn').on('click', function(e) {
+    e.preventDefault();
+    $('.loading-area').hide();
+});
+
 var TEST_LIST_TEMPLATE = $('#test-list-template').html();
 
 function buildTestList(data) {
-	var tests = data.tests;
+    var tests = data.tests;
 
-	for (var i = 0, n = tests.length; i < n; i++) {
-		var listItem = $(tmpl(TEST_LIST_TEMPLATE, tests[i]));
-		$('#test-list').append(listItem);
-	}
+    for (var i = 0, n = tests.length; i < n; i++) {
+        var listItem = $(tmpl(TEST_LIST_TEMPLATE, tests[i]));
+        $('#test-list').append(listItem);
+    }
 }
 
 
